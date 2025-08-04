d60f1965f157906970a1635d1af897d29c862970
sik2 <bickwang123@gmail.com>
2025-08-04 14:30:56 +0900

    lab2

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class App {
@@ -15,5 +17,29 @@ public class App {
         String content = scanner.nextLine();
         System.out.println("cmd : " + cmd);
         System.out.println("content : " + content);
+    }
+
+    void run2() {
+        System.out.println("안녕하세요.");
+
+        // 기존의 output 스트림 저장
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream, true);
+        System.setOut(printStream);
+
+        System.out.println("반갑습니다.");
+        String str1 = byteArrayOutputStream.toString();
+
+        System.out.println("좋아요. 하하");
+        String str2 = byteArrayOutputStream.toString();
+
+        // 기존의 output 스트림 복원
+        System.setOut(ORIGINAL_OUT);
+        printStream.close();
+
+        System.out.println("str1 : " + str1);
+        System.out.println("str2 : " + str2);
     }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,6 +3,7 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         App app = new App();
-        app.run();
+//        app.run();
+        app.run2();
     }
 }
