2f87b8d8e02586bedfa33ee26f6994a637f47d0a
sik2 <bickwang123@gmail.com>
2025-08-04 14:12:35 +0900

    lab 1-1

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,14 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
     void run () {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String cmd = scanner.nextLine();
+
+        System.out.println(cmd);
     }
 }
