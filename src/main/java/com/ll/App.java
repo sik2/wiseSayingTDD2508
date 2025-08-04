3a75b4cd48d93ea8e7b8e5c4f19456b60b858bef
sik2 <bickwang123@gmail.com>
2025-08-04 14:19:35 +0900

    lab 1-4

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class App {
     void run () {
-        String input = "등록";
+        String input = "등록\n 안녕!!";
 
         Scanner scanner = new Scanner(input);
 
         String cmd = scanner.nextLine();
         String content = scanner.nextLine();
-        System.out.println(cmd);
+        System.out.println("cmd : " + cmd);
+        System.out.println("content : " + content);
     }
 }
