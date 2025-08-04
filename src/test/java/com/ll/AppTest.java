feadde491bfa49426e7e649a9f4300fe72db3015
sik2 <bickwang123@gmail.com>
2025-08-04 15:44:25 +0900

    step4 r

--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -13,4 +13,20 @@ public class AppTest {
         String rs = AppTestRunner.run("종료");
         assertThat(rs).contains("== 명언 앱 ==");
     }
+
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """);
+
+        assertThat(rs)
+                .contains("명령) ")
+                .contains("명언 : ")
+                .contains("작가 : ");
+    }
 }
