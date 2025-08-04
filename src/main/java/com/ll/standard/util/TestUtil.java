aa1c60eccb35eaee0abed8e00f4e4b53c1299a45
sik2 <bickwang123@gmail.com>
2025-08-04 14:49:31 +0900

    step1 g

---
+++ src/main/java/com/ll/standard/util/TestUtil.java
@@ -0,0 +1,9 @@
+package com.ll.standard.util;
+
+import java.util.Scanner;
+
+public class TestUtil {
+    public static Scanner genScanner(String input) {
+        return new Scanner(input);
+    }
+}
--- src/test/java/com/ll/standard/util/TestUtilTest.java
+++ src/test/java/com/ll/standard/util/TestUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Scanner;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class TestUtilTest {
 
     @Test
@@ -15,14 +17,13 @@ public class TestUtilTest {
                 나의 죽음을 적들에게 알리지 말라!
                 이순신
                 """);
+
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+        String author = scanner.nextLine();
+
+        assertThat(cmd).isEqualTo("등록");
+        assertThat(content).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
+        assertThat(author).isEqualTo("이순신");
     }
-
-    String cmd = scanner.nextLine();
-    String content = scanner.nextLine();
-    String author = scanner.nextLine();
-
-    assertThat(cmd).isEqualTo("등록");
-    assertThat(cmd).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
-    assertThat(cmd).isEqualTo("이순신");
-
 }
