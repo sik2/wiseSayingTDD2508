d793424e96ed6c9fb5b18e6b17a40a431dfb8483
sik2 <bickwang123@gmail.com>
2025-08-05 09:43:14 +0900

    step6 g

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -10,6 +10,7 @@ public class App {
     }
 
     public void run () {
+        int lastId = 0;
         System.out.println("== 명언 앱 ==");
 
         while (true) {
@@ -24,7 +25,9 @@ public class App {
                     System.out.println("작가 : ");
                     String author = scanner.nextLine();
 
-                    System.out.println("1번 명언이 등록되었습니다.");
+                    int id = ++lastId;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                 }
                 case "종료" -> {
                     return;
