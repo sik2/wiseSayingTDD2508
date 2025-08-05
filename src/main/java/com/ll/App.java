5b9e83151cb5384e7bba64cd0f5ea5639437996f
sik2 <bickwang123@gmail.com>
2025-08-05 09:10:03 +0900

    step4 g

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -11,5 +11,25 @@ public class App {
 
     public void run () {
         System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.println("명령) ");
+            String cmd = scanner.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.println("명언 : ");
+                    String content = scanner.nextLine();
+
+                    System.out.println("작가 : ");
+                    String author = scanner.nextLine();
+                }
+                case "종료" -> {
+                    return;
+                }
+            }
+
+        }
+
     }
 }
