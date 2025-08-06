5e3f5bedaab6425ef37c94bf7b59e8e88671d404
sik2 <bickwang123@gmail.com>
2025-08-06 12:36:10 +0900

    30, r

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -20,4 +20,54 @@ public class UtilTest {
 
         Util.file.delete(filePath);
     }
+
+    @Test
+    @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
+    public void t2() {
+        // given
+        String filePath = "temp/test.txt";
+
+        // when
+        Util.file.set(filePath, "내용");
+
+        // then
+        assertThat(
+                Util.file.get(filePath, "")
+        ).isEqualTo("내용");
+
+        Util.file.delete(filePath);
+    }
+
+    @Test
+    @DisplayName("파일을 삭제할 수 있다.")
+    public void t3() {
+        // given
+        String filePath = "temp/test.txt";
+
+        // when
+        Util.file.touch(filePath);
+        Util.file.delete(filePath);
+
+        // then
+        assertThat(
+                Util.file.notExists(filePath)
+        ).isTrue();
+    }
+
+    @Test
+    @DisplayName("파일을 생성할 수 있다, 만약 해당 경로의 폴더가 없다면 만든다.")
+    public void t4() {
+        // given
+        String filePath = "temp/temp/test.txt";
+
+        // when
+        Util.file.touch(filePath);
+
+        // then
+        assertThat(
+                Util.file.exists(filePath)
+        ).isTrue();
+
+        Util.file.delete(filePath);
+    }
 }
