d5013a1ec68cbc4ac63ed6126577fb5fbb0a82ba
sik2 <bickwang123@gmail.com>
2025-08-06 14:17:00 +0900

    32, r

---
+++ src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -0,0 +1,13 @@
+package com.ll.domain.wiseSaying.repository;
+
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+
+public class WiseSayingFileRepository {
+    public void save(WiseSaying wiseSaying) {
+
+    }
+
+     public WiseSaying findById(int id) {
+         return null;
+     }
+}
---
+++ src/test/java/com/ll/wiseSaying/Repository/WiseSayingRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.ll.wiseSaying.Repository;
+
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+import com.ll.domain.wiseSaying.repository.WiseSayingFileRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class WiseSayingRepositoryTest {
+    private final WiseSayingFileRepository wiseSayingFileRepository;
+
+    WiseSayingRepositoryTest() {
+        wiseSayingFileRepository = new WiseSayingFileRepository();
+    }
+
+    @Test
+    @DisplayName("저장")
+    void t1 () {
+        WiseSaying wiseSaying = new WiseSaying("니체",  "나를 죽이지 못하는 고통은 나를 성장시킬 뿐이다.");
+
+        wiseSayingFileRepository.save(wiseSaying);
+
+        WiseSaying foundWiseSaying =  wiseSayingFileRepository.findById(1);
+
+        assertThat(
+                foundWiseSaying
+        ).isEqualTo(wiseSaying);
+    }
+
+}
